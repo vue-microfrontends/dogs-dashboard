a8011956f1c1e2fd3c6f82a484da3a57b170941c
Joel Denning <joeldenning@gmail.com>
2020-01-18 14:24:05 -0700

    Implementing dog dashboard

--- src/components/dog-dashboard.vue
+++ src/components/dog-dashboard.vue
@@ -1,14 +1,50 @@
 <template>
   <div>
     <page-header title="Dogs dashboard"></page-header>
-    <div>Dashboard!</div>
+    <div class="card">
+      <div v-if="dogs.length === 0">
+        There are no dogs!
+        <router-link to="/rate-doggos">Rate dogs</router-link>.
+      </div>
+      <div v-else class="dogs">
+        <div v-for="dog in dogs" v-bind:key="dog.url" class="dog">
+          <img :src="dog.url" alt="dog photo" />
+          <div class="rating">Rating: {{ dog.rating }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { PageHeader } from "@vue-mf/styleguide";
 
 export default {
-  components: { PageHeader }
+  components: { PageHeader },
+  data: () => ({
+    dogs: JSON.parse(localStorage.getItem("dogs")) || []
+  })
 };
 </script>
-<style scoped></style>
+<style scoped>
+.card {
+  background-color: white;
+  margin: 3.2rem auto 0 auto;
+  width: 75%;
+  padding: 2.4rem;
+}
+
+.dogs {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.dog {
+  margin: 1.6rem;
+  max-width: 100rem;
+}
+
+.rating {
+  display: flex;
+  justify-content: center;
+}
+</style>
